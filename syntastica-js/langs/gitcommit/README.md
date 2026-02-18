f71ef50cdac34427f19ee5b150326198ba5e55ef
Ivan Porto Carrero <ivan@flanders.co.nz>
2026-02-17 21:20:00 -0800

    fix(languages): exclude gitcommit from WASM builds
    The gitcommit parser has massive character tables (84K lines with 18
    locales) that cause emscripten to use 80GB+ RAM during compilation.
    
    Set wasm = false in languages.toml to exclude it from JS/WASM builds.
    The parser will still work for native Rust builds.
    
    Changes:
    - syntastica-macros/languages.toml: wasm = false for gitcommit
    - Removed syntastica-js/langs/gitcommit/ directory

--- syntastica-js/langs/gitcommit/README.md
+++
@@ -1,4 +0,0 @@
-# `syntastica-js-gitcommit`
-
-Gitcommit language support for
-[`syntastica-js`](https://www.npmjs.com/package/@syntastica/core).
--- syntastica-js/langs/gitcommit/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![no_std]
-#![warn(rust_2018_idioms)]
-
-use tree_sitter::Language;
-
-syntastica_macros::js_lang_info!();
-syntastica_macros::js_lang_lib!("gitcommit");
